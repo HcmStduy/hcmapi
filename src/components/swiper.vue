d15f4e69f84dd5aab065c7119d0a5d1668d4f073
hcm <760089836@qq.com>
2023-06-24 11:03:16 +0800

    'vue'

---
+++ src/components/swiper.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn btn-primary">我是按钮</button>
+</template>
+
+<script>
+    export default {
+        name: "swiper"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Me.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="ok">
+        {{msg}}
+        <div>
+            <swiper></swiper>
+            <swiper></swiper>
+            <swiper></swiper>
+            <swiper></swiper>
+            <swiper></swiper>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    import swiper from "@/components/swiper.vue";
+    export default {
+        name: "Me",
+        components: {swiper},
+        data: ()=>{
+            return {
+                msg :'个人中心'
+            }
+        },
+
+    }
+</script>
+
+<style scoped>
+    .ok{
+        color: red;
+        font-size: 50px;
+    }
+</style>
